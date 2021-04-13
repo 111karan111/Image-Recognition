6955b863dfefd8a709003d02a63d1d7d819a4369
Karan Gandhi <kag354@rutgers.edu>
2021-04-13 08:06:13 -0400

    Image Recognition -> Dolphin or Not

--- Mlllll/ViewController.swift
+++ Mlllll/ViewController.swift
@@ -6,14 +6,121 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+//Set Current View controller as a delegate of UI VIew Conroller, Image Poicker and Navigation Controller
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,  UINavigationControllerDelegate{
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    let imagePicker = UIImagePickerController()
+    var classificationResults : [VNClassificationObservation] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+    
     }
+    
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion:  nil)
+            
+            guard let ciImage = CIImage(image: image) else {
+               fatalError("Could not convert")
+            }
+            
+            detect(image: ciImage)
+            
+        }
+      
+        
+    }
+    
+    
+    func detect(image : CIImage){
+        
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Could not initialize the model")
+            
+            
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            
+            guard let results = request.results as? [VNClassificationObservation], let topResult = results.first
+            
+            else {
+                fatalError("unexpected")
+            }
+            
+            
+            
+            if topResult.identifier.contains("hotdog"){
+                DispatchQueue.main.async {
+                    self.navigationItem.title = "HotDog!"
+                    self.navigationController?.navigationBar.barTintColor = UIColor.green
+                    self.navigationController?.navigationBar.isTranslucent = false
+                }
+            }
+                else {
+                    
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                }
+                
+            }
+            
+                
+        }
+            
+            
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            do {
+                
+                
+                try handler.perform([request])
+            }
+            
+            catch {
+                print(error)
+                
+            }
+    }
+        
+        
+        
+    
+    
+  
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
 
+    }
+    
+
+        
 }
+
 
